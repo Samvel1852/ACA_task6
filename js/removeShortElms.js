8231280a82ea853489a7d213787ef2a2cb63c6f6
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-06 13:51:27 +0400

    feat: Added feature to remove short elements

---
+++ js/removeShortElms.js
@@ -0,0 +1,12 @@
+// Write a function which remove elements with length <= 3
+
+function remShortElms(inpArr) {
+  let resArr = [];
+  for (item in inpArr) {
+    if (inpArr[item].length > 3) {
+      resArr.push(inpArr[item]);
+    }
+  }
+  return console.log(resArr);
+}
+remShortElms(["kia", "tesla", "bmw", "mercedes"]);
